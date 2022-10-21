632e8a84115943ce4143701e66d5b62c47c62c67
Md Sholayman <mdsholayman998@gmail.com>
2022-10-21 22:46:39 +0600

    card finished

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -11,8 +11,8 @@ const Banner = () => {
           <div className=" max-w-md ">
             <h1 className="mb-5 text-5xl font-bold">DEW DROPS OFFER FOR COLD WINTER</h1>
             <p className="mb-5 text-2xl">25% Off On All Products</p>
-            <button className="btn btn-accent text-slate-900 mx-4 rounded-none px-6  font-bold">Get Started</button>
-            <button className='border shop-btn font-bold text-sm text-base-100 py-3 px-8'> FIND MORE </button>
+            <button className="btn btn-accent text-slate-900 mx-4 py-0.5 rounded-none px-8  font-bold">Get Started</button>
+            <button className='border shop-btn font-bold text-sm text-base-100 py-3.5 mt-2 lg:mt-0 px-9'> FIND MORE </button>
           </div>
         </div>
       </div>
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,64 @@
 import React from "react";
+import './Card.css'
 import card1 from "../../assets/card-1.jpg";
 import card2 from "../../assets/card-2.jpg";
 import card3 from "../../assets/card-3.jpg";
 
 const Card = () => {
   return (
-    <div></div>
+    <div>
+      <div className=" gap-4 grid lg:grid-cols-3 grid-cols-1 mx-5 lg:mx-20  my-5 lg:my-10">
+        {/* card--1 */}
+        <div className="card shadow-xl image-full">
+          <figure className="" >
+            <img   src={card1} alt="Shoes" />
+          </figure>
+          <div className="card-body  card-text">
+          
+            <div className="card-actions ">
+            <h2 className="card-title text-3xl fw-bold text-white">20% Off On Tank Tops</h2>
+            <p className="text-white text-md my-3">If a dog chews shoes whose shoes does he choose?</p>
+            <button className=' card-btn bg-white font-bold text-sm text-black py-3.5 mt-2 lg:mt-0 px-9'>
+               SHOP NOW </button>
+            </div>
+          </div>
+        </div>
+        {/* card----2 */}
+
+        <div className="card shadow-xl image-full">
+          <figure className="" >
+            <img   src={card2} alt="Shoes" />
+          </figure>
+          <div className="card-body  card-text">
+          
+            <div className="card-actions ">
+            <h2 className="card-title text-3xl fw-bold text-white">Latest Eyewear For You</h2>
+            <p className="text-white text-md my-3">If a dog chews shoes whose shoes does he choose?</p>
+            <button className=' card-btn bg-white font-bold text-sm text-black py-3.5 mt-2 lg:mt-0 px-9'>
+               SHOP NOW </button>
+            </div>
+          </div>
+        </div>
+
+        {/* card----3 */}
+        <div className="card shadow-xl image-full">
+          <figure className="" >
+            <img   src={card3} alt="Shoes" />
+          </figure>
+          <div className="card-body  card-text">
+          
+            <div className="card-actions ">
+            <h2 className="card-title text-3xl fw-bold text-white">Let's Lorem Suit Up!</h2>
+            <p className="text-white text-md my-3">If a dog chews shoes whose shoes does he choose?</p>
+            <button className=' card-btn bg-white font-bold text-sm text-black py-3.5 mt-2 lg:mt-0 px-9'>
+               SHOP NOW </button>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
   );
 };
 
