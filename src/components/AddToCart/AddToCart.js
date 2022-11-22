b916da4c4e722ed81abadaf6c94d422758fa0bea
Md Sholayman <mdsholayman998@gmail.com>
2022-11-22 23:29:42 +0600

    added cart page has been completed

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -1,24 +1,34 @@
 import React from 'react';
+import { AiFillPlusCircle} from 'react-icons/ai';
+import { AiFillMinusSquare} from 'react-icons/ai';
+import './AddToCart.css'
+
 
 const AddToCart = ({cartItems , handleAddProducts , handleRemove}) => {
     const totalPrice = cartItems.reduce((price,item)=> price + item.quantity * item.price , 0 )
     return (
-        <div className='cart-items'>
-            <div className='cart-items-header'>
-            Added cart
+        <div className='cart-items text-black'>
+           
+          <div className='flex justify-center font-semibold text-xl py-3 border-b-2 border-black'>  Added Item cart</div>
 
-            {cartItems.length ===0 && (<div>No Items added</div>)}
+            {cartItems.length ===0 && (<div className='text-2xl font-bold flex justify-center'>No Items added</div>)}
 
-          <div>
-            {cartItems.map(item=>(<div key={item.id} className='cart-items-list'>
-            <img src={item.image} alt={item.name} />
-            <div> {item.name} </div>
-            <div>
-                <button onClick={()=>handleAddProducts(item)} >+</button>
-                <button onClick={()=>handleRemove(item)} >-</button>
+          <div >
+            {cartItems.map(item=>(<div key={item.id} className='flex p-2  items-center lg:pl-5 py-2 lg:py-5 lg:ml-10 '>
+            <div className='lg:w-64 w-28'> <img className='lg:w-32 w-20 lg:h-28 h-20' src={item.image} alt={item.name} /> </div>
+            <div className='font-bold lg:text-2xl text-lg lg:w-32 w-20'> 
+             {item.name}
+             </div>
+            <div className='lg:w-32 w-20'>
+                <button className=' lg:text-2xl text-lg mx-1' onClick={()=>handleAddProducts(item)} >
+                    <AiFillPlusCircle />
+                </button>
+                <button className='lg:text-2xl text-lg mx-1' onClick={()=>handleRemove(item)} >
+                    <AiFillMinusSquare />
+                </button>
             </div>
 
-          <div>
+          <div className='font-bold lg:w-32 w-16 '>
             {item.quantity} * ${item.price}
           </div>
 
@@ -26,14 +36,15 @@ const AddToCart = ({cartItems , handleAddProducts , handleRemove}) => {
             
             ) )}
   
-  <div>total price: 
-    <div> ${totalPrice} </div>
-  </div>
+  
 
           </div>
+          <div className='lg:pl-14 pl-2 py-3 border-t-2 border-black font-bold text-xl'>Total price: 
+         ${totalPrice} 
+  </div>
 
             </div>
-        </div>
+       
     );
 };
 
